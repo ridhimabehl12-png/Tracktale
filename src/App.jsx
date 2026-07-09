498a8be5fb831c9c21b29ef12a2b62b1b12611b1
ridhimabehl12-png <ridhimabehl12@gmail.com>
2026-07-10 04:10:21 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TrackTaleGM from './TrackTaleGM'
+
+function App() {
+  return (
+    <div className="w-full max-w-[1200px]">
+      <TrackTaleGM />
+    </div>
+  )
+}
+
+export default App
